5e4ecbd4c5a7d468cd18545a88f217f70b0c25c9
sid2020-coder <skykite20@gmail.com>
2026-07-19 20:16:42 +0530

    second commit on feature branch

--- src/main/java/org/service/Serve.java
+++ src/main/java/org/service/Serve.java
@@ -2,6 +2,7 @@ package org.service;
 
 public class Serve {
     public static void main(String[] args) {
+        System.out.println("Hello World by person-one");
         System.out.println("Hello World");
     }
 }
